060a15e7eb6e28ea6b6c7748880f2ded50538d68
cpscott1 <thacam18gmail.com>
2020-05-14 21:47:57 -0500

    hit MVP

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -26,10 +26,8 @@ print(y)
 # elements in array a. Hint: "foo".upper() is "FOO".
 
 a = ["foo", "bar", "baz"]
-y = []
+y = [name.upper() for name in a]
 
-for item in a:
-    y.append(item.upper())
 
 print(y)
 
@@ -39,8 +37,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
-for item in x:
-    if int(item) % 2 == 0:
-        y.append(x)
+y = [x for item in x if int(item) % 2 == 0]
+
 print(y)
